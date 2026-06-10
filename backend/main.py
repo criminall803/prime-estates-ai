8a73995f076e1765d61c07b14f46e82413d6fde0
Hammad Malik <3whammad@gmail.com>
2026-06-10 11:36:00 +0500

    Add production frontend CROS

--- backend/main.py
+++ backend/main.py
@@ -10,7 +10,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "https://prime-estates-ai-1.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
